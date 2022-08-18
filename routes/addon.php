a077cd49e3dcd2bc14230eca5efdab8c40ef5707
R. Leroi <r.leroi@live.nl>
2022-08-18 17:48:38 +0200

    laravel 9

--- routes/addon.php
+++ routes/addon.php
@@ -1,19 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AddonController;
+use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'addon'], function() {
-    Route::get('/catalog/{type}/{id}.json', 'AddonController@catalog');
-    Route::get('/catalog/{type}/{id}/{extra}.json', 'AddonController@catalog');
-    
-    Route::get('/meta/{type}/{id}.json', 'AddonController@meta');
-    Route::get('/meta/{type}/{id}/{extra}.json', 'AddonController@meta');
-    
-    Route::get('/stream/{type}/{id}.json', 'AddonController@stream');
-    Route::get('/stream/{type}/{id}/{extra}.json', 'AddonController@stream');
-    
-    Route::get('/subtitles/{type}/{id}.json', 'AddonController@subtitles');
-    Route::get('/subtitles/{type}/{id}/{extra}.json', 'AddonController@subtitles');
+Route::get('/catalog/{type}/{id}.json', [AddonController::class, 'catalog']);
+Route::get('/catalog/{type}/{id}/{extra}.json', [AddonController::class, 'catalog']);
 
-    Route::get('/manifest.json', 'AddonController@manifest');
-});
+Route::get('/meta/{type}/{id}.json', [AddonController::class, 'meta']);
+Route::get('/meta/{type}/{id}/{extra}.json', [AddonController::class, 'meta']);
+
+Route::get('/stream/{type}/{id}.json', [AddonController::class, 'stream']);
+Route::get('/stream/{type}/{id}/{extra}.json', [AddonController::class, 'stream']);
+
+Route::get('/subtitles/{type}/{id}.json', [AddonController::class, 'subtitles']);
+Route::get('/subtitles/{type}/{id}/{extra}.json', [AddonController::class, 'subtitles']);
+
+Route::get('/manifest.json', [AddonController::class, 'manifest']);
